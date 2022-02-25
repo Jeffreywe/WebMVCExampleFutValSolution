3bae7e5a2b18a2ece437528ecf2b852674cef168
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-25 11:20:54 -0500

    Add project files.

---
+++ WebMVCEx.FutVal/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVCExFutVal.Models;
+
+namespace WebMVCExFutVal.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            ViewBag.Name = "Mary"; // view bag is a built in dictionary
+            ViewBag.FV = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model) {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
